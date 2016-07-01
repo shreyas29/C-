9257be6000ac5ab234efaeda4b59a1dc5ba2f3e3
Shreyas Patel <shreyaspatel29@gmail.com>
2016-07-01 12:38:28 +0530

    Added file reading program

--- FileReadWithFixedLengthBuffer.cpp
+++ FileReadWithFixedLengthBuffer.cpp
@@ -1,26 +1,34 @@
-#include <iostream>     // std::cout
-#include <fstream>      // std::ifstream
+#include <iostream>
+#include <fstream>
+
 using namespace std;
+
 const int buffer_length = 10;
+
 int main () {
 
-  std::ifstream is ("temp.cpp");
+  ifstream is ("FileReadWithFixedLengthBuffer.cpp");
   if (is) {
    
     // get length of file:
     is.seekg (0, is.end);
     int length = is.tellg();
     length -= 1;
+
+    //move pointer to start of the file
     is.seekg (0, is.beg);
 
     char * buffer = new char [buffer_length];
+
     int loop = length / buffer_length;
     int rem = length % buffer_length;
+
     for(int i=0;i<loop;i++)
 	{	
-	       is.read (buffer,buffer_length);
-	       cout << buffer;
+       is.read (buffer,buffer_length);
+       cout << buffer;
 	}
+
     is.read (buffer,rem);    
     buffer[rem]='\0';
     cout << buffer;
